c836d7b719a452c4616d8d0f12655aff4b7b6507
Gavin Magee <gtmagee@email.arizona.edu>
2020-03-31 00:46:21 -0700

    Beefed up the for loop demo with a loop iterating over a list

--- code/forLoopDemo.go
+++ code/forLoopDemo.go
@@ -6,5 +6,10 @@ func main(){
 	for i := 97; i < 122; i++ {
 		fmt.Printf("%c,", i)
 	}
-	fmt.Printf("%c", 122)
+	fmt.Printf("%c\n", 122)
+	
+	var myArray [4]string = [4]string{"this", "land", "is", "forsaken"}
+	for i, myStr := range myArray {
+		fmt.Println(myStr,i)
+	}
 }
